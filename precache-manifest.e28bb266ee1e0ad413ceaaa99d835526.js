4bff508350e5cb3f11673492f61f459bdceb4a32
vincephung <vincentphung43@yahoo.com>
2020-10-20 23:33:13 -0400

    Updates

---
+++ precache-manifest.e28bb266ee1e0ad413ceaaa99d835526.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a3b49eec38b98edec408f03ffad8c5",
+    "url": "/blog-frontend-admin/index.html"
+  },
+  {
+    "revision": "ea4595008680f8647176",
+    "url": "/blog-frontend-admin/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b6509def3e7751fcf170",
+    "url": "/blog-frontend-admin/static/css/main.d55ae5cb.chunk.css"
+  },
+  {
+    "revision": "ea4595008680f8647176",
+    "url": "/blog-frontend-admin/static/js/2.479d42f5.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/blog-frontend-admin/static/js/2.479d42f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6509def3e7751fcf170",
+    "url": "/blog-frontend-admin/static/js/main.b0eb6f4d.chunk.js"
+  },
+  {
+    "revision": "31c8bd1682febf225c40",
+    "url": "/blog-frontend-admin/static/js/runtime-main.05212333.js"
+  }
+]);
